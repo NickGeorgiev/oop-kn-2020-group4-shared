5cf3f6aa2f84ed251e44711648c14321e8a6b0c4
victoria0110 <61820249+victoria0110@users.noreply.github.com>
2020-03-30 11:57:46 +0300

    Update [RCS-1] Sstring.h

--- student_work/VictoriaAngelova81960/[RCS-1] Sstring.h
+++ student_work/VictoriaAngelova81960/[RCS-1] Sstring.h
@@ -9,12 +9,13 @@ class String {
     String ();
     String (const size_t& _capacity);
     String (const String& other);
+    String& operator = (const String& other);
     ~String ();
     String (const char* array);
     void push (char elem);
-    size_t _size ();
-    size_t _capacity ();
-    bool empty ();
+    size_t _size () const ;
+    size_t _capacity const ();
+    bool empty () const ;
     char& at (const size_t& pos);
     char& front ();
     char& back ();
@@ -24,10 +25,10 @@ class String {
     void resize (const size_t n);
     void resize (const size_t& n, char character);
     char& operator [] (const size_t& pos);
-    operator bool ();
+    operator bool () const ;
     String& operator += (const String& otherString);
-    String operator + (const String& otherString);
-    friend std::ostream& operator << (std::ostream& out, String& s);
+    String operator + (const String& otherString) const ;
+    friend std::ostream& operator << (std::ostream& out, const String& s);
     friend std::istream& operator >> (std::istream& in, String& s);
 };
 
