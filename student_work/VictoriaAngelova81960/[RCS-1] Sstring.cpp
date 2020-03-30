0de1cb215bdf5c3d3b847ae18f099eaaf3d04cb0
victoria0110 <61820249+victoria0110@users.noreply.github.com>
2020-03-30 23:22:29 +0300

    Update [RCS-1] Sstring.cpp

--- student_work/VictoriaAngelova81960/[RCS-1] Sstring.cpp
+++ student_work/VictoriaAngelova81960/[RCS-1] Sstring.cpp
@@ -3,18 +3,28 @@
 #include<cassert>
 #include "Sstring.h"
 
+
+void String::_resize () {
+    _capacity *= 2;
+    char* newStr = new char[_capacity];
+    for(int i=0;i< _size; i++) {
+        newStr[i] = str[i];
+    }
+    delete [] str;
+    str = newStr;
+}
 String::String () : str{nullptr}, size{0}, capacity{0} {}
-String::String (const size_t& _capacity) : capacity{_capacity}, str{new char[_capacity]}, size{0} {}
-String::String (const String& other) : capacity{other.capacity}, size{other.size}, str{new char[other.capacity]} {
+String::String (const size_t& capacity) : _capacity{capacity}, str{new char[capacity]}, _size{0} {}
+String::String (const String& other) : _capacity{other._capacity}, _size{other._size}, str{new char[other._capacity]} {
     strcpy(str, other.str);
 }
 
 String& String::operator = (const String& other) {
     if(this!=&other) {
         delete [] str;
-        capacity=other.capacity;
-        size=other.size;
-        str=new char[other.capacity];
+        _capacity=other._capacity;
+        _size=other._size;
+        str=new char[other._capacity];
         strcpy(str,other.str);
     }
     return *this;
@@ -23,117 +33,138 @@ String& String::operator = (const String& other) {
 String::~String () {
     delete [] str;
     str=nullptr;
-    size=0;
-    capacity=0;
+    _size=0;
+    _capacity=0;
 }
 
 String::String (const char* array) {
-    capacity=(strlen(array)+1)*2;
-    size=strlen(array);
-    str=new char[capacity];
+    _capacity=strlen(array);
+    _size=strlen(array);
+    str=new char[_capacity];
     strcpy(str, array);
-    //ако не можем да използваме cstring
-    /*for(int i=0;i<size;i++) {   
-         str[i]=array[i];
-     }*/
 }
 
 void String::push (char elem) {
-    char* newString=new char[capacity+2];
+    if(_capacity-_size==1) {
+        _resize();
+    }
     strcpy(newString, str);
-    newString[size]=elem;
-    newString[size+1]=0;
-    size++;
-    capacity+=2;
-
+    newString[_size]=elem;
+    newString[_size+1]=0;
+    _size++;
     delete []str;
     str=newString;
 }
 
-size_t String::_size () const {
-    return size;
+size_t String::size () const {
+    return _size;
 }
 
-size_t String::_capacity () const {
-    return capacity;
+size_t String::capacity () const {
+    return _capacity;
 }
 
 bool String::empty () const {
-    return size==0;
+    return _size==0;
 }
 
 char& String::at (const size_t& pos) {
-    assert(size>0);
+    assert(_size>0);
+    assert(pos<_size);
     return str[pos];
 }
 
 char& String::front () {
-    assert(size>0);
+    assert(_size>0);
     return str[0];
 }
 
 char& String::back () {
-    assert(size>0);
-    return str[size-1];
+    assert(_size>0);
+    return str[_size-1];
+}
+
+char String::at (const size_t& pos) const {
+    assert(_size>0);
+    assert(pos<_size);
+    return str[pos];
+}
+
+char String::front () const {
+    assert(_size>0);
+    return str[0];
+}
+
+char String::back () const {
+    assert(_size>0);
+    return str[_size-1];
 }
 
 void String::append (const String& appStr) {
-    capacity+=appStr.capacity;
-    char* newString=new char[capacity];
+    if((_capacity-(_size+appStr._size))==1) {
+        _resize();
+    }
+    char* newString=new char[_capacity];
     strcpy(newString, str);
     strcat(newString, appStr.str);
-    size+=appStr.size;
-    newString[size]=0;
+    _size+=appStr._size;
+    newString[_size]=0;
     delete [] str;
     str=newString;
 }
 
-char* String::c_char () {
-    char* charArray=new char[size+1];
+char* String::c_str () {
+    char* charArray=new char[_size+1];
     strcpy(charArray, str);
     return charArray;
 }
 
 void String::shrink_to_fit () {
-    capacity=size+1;
-    char* newString=new char[capacity+1];
-    strcpy(newString, str);
-    delete [] str;
-    str=newString;
+    if(strlen(str)>0) {
+        _capacity=_size+1;
+        char* newString=new char[_capacity+1];
+        strcpy(newString, str);
+        delete [] str;
+        str=newString;
+    }
 }
 
 void String::resize (const size_t n) {
-    capacity=n*2;
-    char* smallerString=new char[capacity];
-    size=n;
-    for(size_t i=0;i<size;i++) {
+    _capacity=n*2;
+    char* smallerString=new char[_capacity];
+    for(size_t i=0;i<n;i++) {
         smallerString[i]=str[i];
     }
-    smallerString[size]=0;
+    smallerString[n]=0;
     delete []str;
     str=smallerString;
 }
 
 void String::resize (const size_t& n, char character) {
-    capacity=n*2;
-    char* biggerString=new char[capacity];
-    strcpy(biggerString, str);
-    while(size<=n) {
-        size++;
-        biggerString[size-1]=character;
+    if(_capacity-n==1) {
+        _resize();
     }
-    biggerString[size]=0;
+    char* biggerString=new char[_capacity];
+    strcpy(biggerString, str);
+    while(_size<=n) {
+        size++;
+        biggerString[_size-1]=_character;
+    }
+    biggerString[_size]=0;
     delete [] str;
     str=biggerString;
 }
 
 char& String::operator [] (const size_t& pos) { 
-    assert(pos<size);
+    return str[pos];
+}
+
+char String::operator [] (const size_t& pos) const { 
     return str[pos];
 }
 
 String::operator bool () const {
-    return size==0;
+    return _size==0;
 }
 
 String& String::operator += (const String& otherString) {
@@ -144,12 +175,10 @@ String& String::operator += (const String& otherString) {
 String String::operator + (const String& otherString) const {
     String result(*this);
     result+=otherString;
-    std::cout<<result<<std::endl;
     return result;
 }
 
 std::ostream& operator << (std::ostream& out, const String& s) {
-    assert(s.capacity>=s.size);
     out<<s.str;
     return out;
 }
