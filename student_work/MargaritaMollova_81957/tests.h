90a3fb16a04d13dd46aa5cb1ab9402e19c689b6e
mollova <m.mollova21@gmail.com>
2020-04-05 17:28:54 +0300

    changed dependent subcases

--- student_work/MargaritaMollova_81957/tests.h
+++ student_work/MargaritaMollova_81957/tests.h
@@ -3,15 +3,15 @@
 #include "doctest.h"
 #include "String.h"
 
-TEST_CASE ("Operator == test")
+TEST_CASE ("Operator ==")
 {
     String t1("mau"), t2("mau"), t3("bau");
 
-    CHECK (t1==t2);
-    CHECK (t1==t3);
+    CHECK ((bool)(t1==t2));
+    CHECK (!(t1==t3));
 }
 
-TEST_CASE ("Size, Capacity and C_str test")
+TEST_CASE ("Size, Capacity and C_str")
 {
     String t1("mau");
 
@@ -20,14 +20,11 @@ TEST_CASE ("Size, Capacity and C_str test")
     CHECK (!strcmp(t1.c_str(), "mau"));
 }
 
-TEST_CASE ("Constructors test")
+TEST_CASE ("Constructors")
 {
-    String t1("kotka");
-    String t2(t1);
-    String t3;
-
     SUBCASE ("Constuctor with parameters")
     {
+        String t1("kotka");
         CHECK (!strcmp(t1.c_str(),"kotka"));
         CHECK (t1.size() == 5);
         CHECK (t1.capacity() == 10);
@@ -35,52 +32,62 @@ TEST_CASE ("Constructors test")
 
     SUBCASE ("Copy constructor")
     {
-        CHECK (t2 == t1);
+        String t1("kotka"), t2(t1);
+        CHECK ((bool)(t2 == t1));
         CHECK (t2.capacity() == 10);
     }
 
     SUBCASE ("Default constructor")
     {
+        String t3;
         CHECK (t3.size() == 0);
     }
 }
 
-TEST_CASE ("Operator = test")
+TEST_CASE ("Operator =")
 {
     String t1("kotka");
     String t2;
 
     t2 = t1;
-    CHECK (t2 == t1);
+    CHECK ((bool)(t2 == t1));
 }
 
-TEST_CASE ("Append test")
+TEST_CASE ("Append")
 {
     String t1("ko");
     String t2("tka");
     String t3("kotka");
 
     t1.append(t2);
-    CHECK (t1 == t3);
+    CHECK ((bool)(t1 == t3));
 }
 
-TEST_CASE ("Operaror + and Operator += test")
+TEST_CASE ("Operaror +")
 {
     String t1("kotka");
     String t2("kotkata");
     String t3("ta");
 
-    CHECK (t1+t3 == t2);
-    t1+=t3;
-    CHECK (t1 == t2);
+    CHECK ((bool)(t1+t3 == t2));
 }
 
-TEST_CASE ("At, front, back and Operator [] test")
+TEST_CASE ("Operator +=")
 {
     String t1("kotka");
+    String t2("kotkata");
+    String t3("ta");
 
+    t1+=t3;
+    CHECK ((bool)(t1 == t2));
+}
+
+TEST_CASE ("At, front, back and Operator []")
+{
     SUBCASE ("Only accessing the elements")
     {
+        String t1("kotka");
+
         CHECK (t1.at(2) == 't');
         CHECK (t1.front() == 'k');
         CHECK (t1.back() == 'a');
@@ -89,13 +96,14 @@ TEST_CASE ("At, front, back and Operator [] test")
 
     SUBCASE ("Changing the elements")
     {
+        String t1("kotka");
+
         t1.at(2) = 'i';
         t1.front() = 'p';
         t1.back() = 'i';
         t1[1] = 'u';
         CHECK (!strcmp(t1.c_str(), "puiki"));
     }
-
 
     SUBCASE ("Constant string")
     {
@@ -108,7 +116,7 @@ TEST_CASE ("At, front, back and Operator [] test")
     }
 }
 
-TEST_CASE ("Push test")
+TEST_CASE ("Push")
 {
     String t1;
     t1.push('a');
@@ -119,25 +127,25 @@ TEST_CASE ("Push test")
     CHECK (t1[2] == 'c');
 }
 
-TEST_CASE ("Empty and Operator bool test")
+TEST_CASE ("Empty and Operator bool")
 {
-    String t1;
-
     SUBCASE ("When empty")
     {
+        String t1;
         CHECK (t1.empty());
         CHECK (t1);
     }
 
     SUBCASE ("When full")
     {
+        String t1;
         t1.push('a');
         CHECK (!t1.empty());
         CHECK (!t1);
     }
 }
 
-TEST_CASE ("Shrink to fit test")
+TEST_CASE ("Shrink to fit")
 {
     String t1("kotka");
     t1.shrink_to_fit();
@@ -146,41 +154,46 @@ TEST_CASE ("Shrink to fit test")
     CHECK (t1.capacity() == 6);
 }
 
-TEST_CASE ("Resize(n) and Resize(n, character) test")
+TEST_CASE ("Resize(n)")
 {
-    String t1("kotkata");
-
-    SUBCASE ("Resize(n) when n<size")
+    SUBCASE ("When n<=size")
     {
+        String t1("kotkata");
+
         t1.resize(5);
         CHECK (t1.size() == 5);
         CHECK (t1[4] == 'a');
     }
 
-    t1.shrink_to_fit();
-
-    SUBCASE ("Resize(n) when n>size")
+    SUBCASE ("When n>size")
     {
-        t1.resize(8);
-        CHECK (t1.size() == 8);
-        CHECK (t1[7] == ' ');
-        CHECK (t1.capacity() == 16);
+        String t("kotkata");
+
+        t.resize(10);
+        CHECK (t.size() == 7);
+        CHECK (t[4] == 'a');
+        CHECK (t.capacity() == 14);
     }
+}
 
-    SUBCASE ("Resize(n, character) when n<size")
+TEST_CASE ("Resize(n, character)")
+{
+    SUBCASE ("When n<=size")
     {
+        String t1("kotkata");
+
         t1.resize(4,'a');
         CHECK (t1.size() == 4);
         CHECK (t1[3] == 'k');
     }
 
-    t1.shrink_to_fit();
-
-    SUBCASE ("Resize(n, character) when n>size")
+    SUBCASE ("When n>size")
     {
-        t1.resize(10,'i');
-        CHECK (t1.size() == 10);
-        CHECK (t1[9] == 'i');
-        CHECK (t1.capacity() == 20);
+        String t1("kotka");
+
+        t1.resize(11,'i');
+        CHECK (t1.size() == 11);
+        CHECK (t1[10] == 'i');
+        CHECK (t1.capacity() == 22);
     }
 }
