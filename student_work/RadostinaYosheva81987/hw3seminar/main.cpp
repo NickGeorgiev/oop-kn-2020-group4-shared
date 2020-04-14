66d92907c3e6263577dc8fe41b397ebdd1f7e2d5
Radostina <r.yosheva@gmail.com>
2020-04-14 22:54:07 +0300

    Homework 3

---
+++ student_work/RadostinaYosheva81987/hw3seminar/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "LList.h"
+
+// int main()
+// {
+	// LList list;
+
+    // int arr[2] = {1, 2};
+
+    // list.create(arr, 2);
+
+	// list.addNewEnd(3);
+	// list.addNewEnd(2);
+	// list.addNewEnd(1);
+
+	// list.print();
+	// std::cout << std::endl;
+	// list.reverse();
+	// list.print();
+
+//     return 0;
+// }
